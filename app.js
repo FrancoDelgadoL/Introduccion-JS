9ad29c3d132cf61efa5da772a75a3cef46c36e2b
Franco Delgado <franco1801@hotmail.com>
2024-05-08 23:06:28 -0500

    Introduccion a JS

---
+++ app.js
@@ -0,0 +1,21 @@
+// Creacion de variables en Javascript // 
+
+// Con la version ES6 hay dos maneras de crear variables //
+
+// LET
+//CONST
+
+//LET nos permite crear variables sin necesidad de asignarle valor
+
+//Ejemplos de LET
+
+let fullName = 'Franco Delgado'; 
+let age = 30;
+let greetings;
+
+//CONST a diferencia de LET, tenemos que asiganrle un valor a la variable si o si, y son valores que no van a cambiar
+
+//Ejemplos de CONST
+
+const year = 2024;
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Javascript
+Javascript es un lenguaje de programacion que podemos aplicar en este caso a nuestras paginas web para hacerlas interactivas. 
+
+## Creacion de variales 
+Desde la version ES6 tenemos dos formas de crear variables
+- LET
+- CONST
+
+Ambas tienen sus diferencias dependiendo del tipo de variable que vayamos a crear
